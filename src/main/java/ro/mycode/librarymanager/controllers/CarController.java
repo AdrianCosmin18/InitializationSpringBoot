e26f9f554c89ab3f8bffe463b53ae464534b1e2b
nedelcucosminadrian <nedelcuadriancosmin@gmail.com>
2022-08-23 19:22:23 +0300

    3/15

---
+++ src/main/java/ro/mycode/librarymanager/controllers/CarController.java
@@ -0,0 +1,97 @@
+package ro.mycode.librarymanager.controllers;
+
+
+import org.springframework.web.bind.annotation.*;
+import ro.mycode.librarymanager.models.Car;
+import ro.mycode.librarymanager.respository.CarRepo;
+
+import java.util.List;
+
+@RestController
+public class CarController {
+
+    private CarRepo carRepo;
+
+    public CarController(CarRepo carRepo) {
+        this.carRepo = carRepo;
+    }
+
+    @GetMapping("/toate-masinile")
+    public List<Car> getAllCars(){
+        return  carRepo.findAll();
+    }
+
+    @PostMapping("/adauga-masina")
+    public void addCar(@RequestBody Car car){
+        carRepo.save(car);
+    }
+
+    @DeleteMapping("/delete-car/{id}")
+    public void eraseCar(@PathVariable Long id){
+        carRepo.deleteById(id);
+    }
+
+    @PutMapping("/update-car/{id}")
+    public Car update(@PathVariable Long id, @RequestBody Car car){
+
+        Car existingCar = carRepo.findById(id).get();
+        existingCar.setBrand(car.getBrand());
+        existingCar.setModel(car.getModel());
+        existingCar.setColor(car.getColor());
+        existingCar.setWeight(car.getWeight());
+        existingCar.setYear(car.getYear());
+        carRepo.save(existingCar);
+        return existingCar;
+    }
+
+    //ce returneaza masina cea mai noua
+    @GetMapping("/max-year")
+    public Car getMaxYear(){
+        return carRepo.oldestCar();
+    }
+
+    //5 functionati
+
+    @GetMapping("/all-cars-bigger-than")
+    public List<Car> getCarsByMinimumYear(@RequestParam(value = "year") Integer year){
+        return carRepo.getCarsByMinimumYear(year);
+    }
+
+    @GetMapping("/all-cars-by-max-weight/{maxWeight}")
+    public List<Car> getCarByMaxWeight(@PathVariable double maxWeight){
+        return carRepo.findCarsByWeightBefore(maxWeight);
+    }
+
+    @DeleteMapping("/delete-by-max-and-min-weight")
+    public void delete (@RequestParam(value = "minWeight") Double minWeight, @RequestParam(value = "maxWeight") Double maxWeight){
+
+        carRepo.deleteCars(minWeight, maxWeight);
+    }
+
+    @PutMapping("/update-car-color-by-brand")
+    public void updateColorByBrand(@RequestParam(value = "brand") String brand, @RequestParam(value = "color") String color){
+
+        carRepo.updateCarColorByBrand(brand, color);
+    }
+
+    //tema 1)
+    @PutMapping("/update-car-color-weight-by-id/{id}")
+    public void updateColorAndWeightById(@PathVariable Long id, @RequestParam(value = "color") String color, @RequestParam(value = "weight") Double weight){
+        carRepo.updateColorAndWeightById(id, color, weight);
+    }
+    //2)
+    @DeleteMapping("/delete-car-by-model-and-year")
+    public void deleteByModelAndYear(@RequestParam(value = "model") String model, @RequestParam(value = "year") int year){
+        carRepo.deleteCarsByModelAndYear(model, year);
+    }
+
+    //3)
+    @PutMapping("/update-car2/{id}")
+    public void updateCarById(@PathVariable long id, @RequestBody Car car){
+        carRepo.updateCarById(id, car.getBrand(), car.getModel(), car.getYear(), car.getColor(), car.getWeight());
+    }
+
+    //4
+
+
+}
--- src/main/java/ro/mycode/librarymanager/respository/CarRepo.java
+++ src/main/java/ro/mycode/librarymanager/respository/CarRepo.java
@@ -1,7 +1,47 @@
 package ro.mycode.librarymanager.respository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import ro.mycode.librarymanager.models.Car;
 
+import java.util.List;
+
+
+@Repository
+@Transactional
 public interface CarRepo extends JpaRepository<Car, Long> {
+
+    @Query("select c from Car c where  c.year in (SELECT max(t.year) from Car t)")
+    Car oldestCar();
+
+    @Query("select c from Car c where c.year >= ?1")
+    List<Car> getCarsByMinimumYear(int year);
+
+    List<Car> findCarsByWeightBefore(double maxWeight);
+
+    @Modifying
+    @Query("delete from Car c where c.weight between ?1 and ?2")
+    void deleteCars(double min, double max);
+
+    @Modifying
+    @Query("update Car c set c.color = :col where c.brand = :br")
+    void updateCarColorByBrand(String br, String col);
+
+    //tema
+    @Modifying
+    @Query("update Car c set c.color = :col, c.weight = :wgh where c.id = :id")
+    void updateColorAndWeightById(long id, String col, double wgh);
+
+    @Modifying //nu merge cu queryDSL
+    @Query("delete from Car c where c.model = :m and c.year = :y")
+    void deleteCarsByModelAndYear(String m, int y);
+
+    @Modifying
+    @Query("update Car c set c.brand = :brand, c.weight = :weight, c.model = :model, c.year = :year, c.color = :color where c.id = :id")
+    void updateCarById(long id, String brand, String model, int year, String color, double weight);
+
 }
